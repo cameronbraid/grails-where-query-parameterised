9e5915278b83967aa17b6bfc582a2d9855d43dcd
cameronbraid <cameronbraid@beast.braid.com.au>
2011-11-18 23:02:46 +1000

    fixed sorting

--- grails-app/controllers/demo/DemoController.groovy
+++ grails-app/controllers/demo/DemoController.groovy
@@ -6,14 +6,14 @@ class DemoController {
 
 	def index() {
 		def mainCategory = Category.findAllByName("main").get(0)
-		def q = User.where { category == mainCategory }.sort("username", "desc").sort("username", "asc")
+		def q = User.where { category == mainCategory }.sort("username", "desc")
 		def users = q.list()
 		render "Found ${users.size()} users"
 	}
 
 	def index2() {
 		def mainCategory = Category.findAllByName("main").get(0)
-		def q = User.where { username != 'test' && category == mainCategory }.sort("username", "desc").sort("username", "asc")
+		def q = User.where { username != 'test' && category == mainCategory }.sort("username", "desc")
 		def users = q.list()
 		render "Found ${users.size()} users"
 	}
